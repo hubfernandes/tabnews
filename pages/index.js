42a0dcfe852647ce5d6b3d33eee1f322a65cda93
hubfernandes <115116372+hubfernandes@users.noreply.github.com>
2025-05-14 00:15:00 +0000

    S2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Será que o deploy vai mesmo funcionar?</h1>;
+  return (
+    <h1>
+      {" "}
+      Clarinha, só para não perder a oportunidade e dizer que te amo muito! /n
+      Vc é muito especial para mim ❤
+    </h1>
+  );
 }
 
 export default Home;
