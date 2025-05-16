139cb832ce2e8ed651fb146ca727253a219f9a54
hubfernandes <115116372+hubfernandes@users.noreply.github.com>
2025-05-16 20:34:41 +0000

    add api/v1/status fields

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,38 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const dbName = process.env.POSTGRES_DB;
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsValue = parseInt(
+    dbMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  console.log(dbOpenedConnectionsResult.rows[0]);
 
   response.status(200).json({
-    k: "v",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConnectionsValue,
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
 test("GET: /api/v1/status => 200", async () => {
   var response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  var body = await response.json();
+
+  const parsed = new Date(body.updated_at).toISOString();
+
+  expect(body.updated_at).toBe(parsed);
+
+  const database = body.dependencies.database;
+
+  const dbVersion = database.version;
+
+  expect(dbVersion).toBe("16.0");
+
+  const dbMaxConnections = database.max_connections;
+
+  expect(dbMaxConnections).toBe(100);
+
+  const dbOpennedConnections = database.opened_connections;
+
+  expect(dbOpennedConnections).toBe(1);
 });
