ea3e30461604b9db67faa8f8047742a58d3296aa
hubfernandes <115116372+hubfernandes@users.noreply.github.com>
2026-01-28 12:59:51 +0000

    add facebook head metadata

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
+
 function Home() {
   return (
     <html>
-      <head>
-        <title>Lucernos</title>
-      </head>
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="4z8f7zuqlymkb9ux32u6h51dgk5jlp"
+        />
+      </Head>
       <body>
         <p>Project Lucernos is coming soom!</p>
       </body>
@@ -12,12 +17,3 @@ function Home() {
 }
 
 export default Home;
-
-export const metadata = {
-  title: "Lucernos",
-  verification: {
-    other: {
-      "facebook-domain-verification": ["4z8f7zuqlymkb9ux32u6h51dgk5jlp"],
-    },
-  },
-};
