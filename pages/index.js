fa1c25fe6c4d3fae587cb832e0559726c12918c2
hubfernandes <115116372+hubfernandes@users.noreply.github.com>
2026-01-28 13:01:59 +0000

    update meta headers

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,16 @@ import Head from "next/head";
 
 function Home() {
   return (
-    <html>
+    <div>
       <Head>
+        <title>Lucernos</title>
         <meta
           name="facebook-domain-verification"
           content="4z8f7zuqlymkb9ux32u6h51dgk5jlp"
         />
       </Head>
-      <body>
-        <p>Project Lucernos is coming soom!</p>
-      </body>
-    </html>
+      <h1>Lucernos is coming soon!</h1>
+    </div>
   );
 }
 
